ad266b37c104ea69ae4089665e3192f1eb1994e9
Vikrant4239 <bhardwazvikrant83@gmail.com>
2023-02-04 15:14:54 +0530

    by tagname and query selector

--- dom.js
+++ dom.js
@@ -4,8 +4,21 @@
 //manipulating.style.border='2px solid black';
 //manipulating.style.backgroundColor='grey';
 
-let man = document.getElementsByClassName('list-group-item');
+/*let man = document.getElementsByClassName('list-group-item');
 
 for(let i=0;i<man.length;i++){
     man[i].style.backgroundColor="grey";
+}*/
+
+let man = document.getElementsByTagName('li');
+
+for(let i=0;i<man.length;i++){
+ man[i].style.backgroundColor="grey";
 }
+
+let nam = document.querySelector('li');
+ nam.style.background="yellow";
+
+ let sam = document.querySelector('.btn');
+ sam.style.backgroundColor="green";
+ sam.value='jao';
