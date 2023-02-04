b5339b3ce3266a89f9262967fe222da668d4b22b
Vikrant4239 <bhardwazvikrant83@gmail.com>
2023-02-04 12:47:30 +0530

    js file

---
+++ dom.js
@@ -0,0 +1,5 @@
+console.log(document.getElementById('heading'));
+let manipulating = document.getElementById('heading');
+//manipulating.textContent='Hello';
+manipulating.style.border='2px solid black';
+manipulating.style.backgroundColor='grey';
